4a0a1ebb60136936e2a2be0b40377282d72e6b65
Jacques Chester <jacques@chester.id.au>
2016-10-17 20:43:23 -0400

    Add 'flaky' task

---
+++ 002-basics-of-reading-a-concourse-pipeline/tasks/flaky/run.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+# This script simulates a flaky test by randomly
+# "failing" approximately 1/8th of the time.
+
+puts
+puts 'Running test!'
+
+if Random.rand(8) == 8
+  puts 'The test failed! All is lost!'
+  exit 1
+else
+  puts 'Test OK!'
+  exit 0
+end
