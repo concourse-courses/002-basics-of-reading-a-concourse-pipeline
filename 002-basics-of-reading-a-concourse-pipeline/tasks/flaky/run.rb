5e5f365608aec74c7f12cbefddc753a0f64b612a
Jacques Chester <jacques@chester.id.au>
2016-10-23 11:37:10 -0400

    Add ALWAYS_PASS for flaky task

--- 002-basics-of-reading-a-concourse-pipeline/tasks/flaky/run.rb
+++ 002-basics-of-reading-a-concourse-pipeline/tasks/flaky/run.rb
@@ -6,10 +6,21 @@
 puts
 puts 'Running test!'
 
-if ENV['ALWAYS_FAIL'] == 'true' || Random.rand(8) == 7
-  puts 'The test failed! All is lost!'
-  exit 1
-else
+def test_ok
   puts 'Test OK!'
   exit 0
+end
+
+def test_fail
+  puts 'The test failed! All is lost!'
+  exit 1
+end
+
+test_ok     if ENV['ALWAYS_PASS'] == 'true'
+test_fail   if ENV['ALWAYS_FAIL'] == 'true'
+
+if Random.rand(8) != 7
+  test_ok
+else
+  test_fail
 end
